b12ce41d7ff3a1a131cd263f8ee8e50b97d1ed3d
Osimitzu <angel_aocc@hotmail.com>
2021-12-10 12:00:30 -0600

    menuHamburguesaIncompleto

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+const iconoMenu = document.querySelector('#iconoMenu'),
+    menu =document.querySelector('#menu');
+
+iconoMenu.addEventListener('click', (e) => {
+    //Alternar estilos para el menu y el body
+    menu.classList.toggle('active');
+    document.body.classList.toggle('opacity');
+
+    //Alternar atributo "src" para el icono del menú
+    const rutaActual = e.target.getAttribute('src');
+
+    if (rutaActual == 'fas fa-bars fa-3x'){
+        e.target.setAttribute('src', 'fas fa-bars2 fa-3x');
+    }else{
+        e.target.setAttribute('src', 'fas fa-bars fa-3x');
+    }
+});
